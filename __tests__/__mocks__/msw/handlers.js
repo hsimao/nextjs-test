115337b2ca61f3c2f025f2ed5b1f3aabda848e2a
hsimao <e087754958@gmail.com>
2022-10-06 20:26:43 +0800

    feat: test user reservations button

--- __tests__/__mocks__/msw/handlers.js
+++ __tests__/__mocks__/msw/handlers.js
@@ -1,9 +1,20 @@
 import { rest } from "msw";
 import { readFakeData } from "@/__tests__/__mocks__/fakeData";
+import { fakeUserReservations } from "@/__tests__/__mocks__/fakeData/userReservations";
 
 export const handlers = [
   rest.get("http://localhost:3000/api/shows/:showId", async (req, res, ctx) => {
     const { fakeShows } = await readFakeData();
     return res(ctx.json({ show: fakeShows[0] }));
   }),
+  rest.get(
+    "http://localhost:3000/api/users/:userId/reservations",
+    (req, res, ctx) => {
+      const { userId } = req.params;
+
+      // return fakeUserReservations if userId is 1; empty array otherwise
+      const userReservations = Number(userId) === 1 ? fakeUserReservations : [];
+      return res(ctx.json({ userReservations }));
+    }
+  ),
 ];
